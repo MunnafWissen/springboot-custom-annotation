c7a9f7212f2d03b37cfea48711e79ea6f5bc92ce
MunnafWissen <koilakuntla.munnaf@wissen.com>
2026-04-17 11:31:07 +0530

    Demo2 added in Employee

--- custom-annotation-project/src/main/java/com/example/customannotationproject/model/Employee.java
+++ custom-annotation-project/src/main/java/com/example/customannotationproject/model/Employee.java
@@ -6,7 +6,7 @@ import com.example.customannotationproject.NameValidator;
 public class Employee {
 
     private int id;
-    private String Demo;
+    private String Demo2;
     private String firstName;
     private String middleName;
     private String lastName;
