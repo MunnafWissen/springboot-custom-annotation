fe4039c6ada945c6ff10d9827801a2f764ed97a4
MunnafWissen <koilakuntla.munnaf@wissen.com>
2026-04-27 15:35:41 +0530

    Delete kafka-consumer/src/main directory

--- kafka-consumer/src/main/java/com/example/entitygraph/kafkaconsumer/repo/EmpRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.entitygraph.kafkaconsumer.repo;
-
-import com.example.entitygraph.kafkaconsumer.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmpRepo extends JpaRepository<Employee, Integer> {
-}
